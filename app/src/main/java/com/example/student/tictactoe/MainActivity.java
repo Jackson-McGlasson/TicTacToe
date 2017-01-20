af0ca6168ce2b9efd4a539d06230e7a9d306c1c4
Jackson Mcglasson <Jackson-McGlasson@users.noreply.github.com>
2017-01-20 13:25:13 -0800

    three in arow

--- app/src/main/java/com/example/student/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/student/tictactoe/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.student.tictactoe;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
     TextView button8Var;
     TextView button9Var;
     int clickState = 0;
+    int x1 = 0;
+    int x2 = 0;
+    int x3= 0;
+    int x4 = 0;
+    int x5 = 0;
+    int x6 = 0;
+    int x7= 0;
+    int x8 = 0;
+    int x9 = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,104 +45,133 @@ public class MainActivity extends AppCompatActivity {
         button8Var = (TextView) findViewById(R.id.button8);
         button9Var = (TextView) findViewById(R.id.button9);
     }
+    private void check3inarow(){
+        if (x1==x2 && x2==x3){
+            CharSequence text = "This text will appear";
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            Toast myMessage= Toast.makeText(context, text, duration);
+            myMessage.show();
+        }
+        if (x1==x3 && x3==x7){
+            CharSequence text = "This text will appear";
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            Toast myMessage= Toast.makeText(context, text, duration);
+            myMessage.show();
+        }
+    }
+
 
     public void click1(View view){
-        if(clickState == 0){
+        if(x1 == 0){
             button1Var.setText("O");
-            clickState=1;
+            x1=1;
+
+            check3inarow(); //This calls the checker function
         }
         else {
             button1Var.setText("X");
-            clickState=0;
+            x1=0;
         }
-
     }
+
     public void click2(View view){
-        if(clickState == 0){
+        if(x2 == 0){
             button2Var.setText("O");
-            clickState=1;
+            x2=1;
+            check3inarow();
         }
         else {
             button2Var.setText("X");
-            clickState=0;
+            x2=0;
         }
 
     }
     public void click3(View view){
-        if(clickState == 0){
+        if(x3 == 0){
             button3Var.setText("O");
-            clickState=1;
+            x3=1;
+            check3inarow();
         }
         else {
             button3Var.setText("X");
-            clickState=0;
+            x3=0;
         }
 
     }
     public void click4(View view){
-        if(clickState == 0){
+        if(x4 == 0){
             button4Var.setText("O");
-            clickState=1;
+            x4=1;
+            check3inarow();
         }
         else {
             button4Var.setText("X");
-            clickState=0;
+            x4=0;
         }
 
     }
     public void click5(View view){
-        if(clickState == 0){
+        if(x5 == 0){
             button5Var.setText("O");
-            clickState=1;
+            x5=1;
+            check3inarow();
         }
         else {
             button5Var.setText("X");
-            clickState=0;
+            x5=0;
         }
 
     }
     public void click6(View view){
-        if(clickState == 0){
+        if(x6 == 0){
             button6Var.setText("O");
-            clickState=1;
+            x6=1;
+            check3inarow();
         }
         else {
             button6Var.setText("X");
-            clickState=0;
+            x6=0;
         }
 
     }
     public void click7(View view){
-        if(clickState == 0){
+        if(x7 == 0){
             button7Var.setText("O");
-            clickState=1;
+            x7=1;
+            check3inarow();
         }
         else {
             button7Var.setText("X");
-            clickState=0;
+            x7=0;
         }
 
     }
     public void click8(View view){
-        if(clickState == 0){
+        if(x8 == 0){
             button8Var.setText("O");
-            clickState=1;
+            x8=1;
+            check3inarow();
         }
         else {
             button8Var.setText("X");
-            clickState=0;
+            x8=0;
         }
 
     }
     public void click9(View view){
-        if(clickState == 0){
+        if(x9 == 0){
             button9Var.setText("O");
-            clickState=1;
+            x9=1;
         }
         else {
             button9Var.setText("X");
-            clickState=0;
+            x9=0;
         }
 
     }
+
 }
+
+
